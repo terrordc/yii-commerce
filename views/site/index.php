a761827b83d341fa36795c0010afb60cf4fa148c
riiriri <ijsdfijjifsd@mail.ru>
2023-04-15 12:32:47 +0600

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -13,7 +13,7 @@ $this->title = 'My Yii Application';
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
-
+asdasdasdasd
     <div class="body-content">
 
 
