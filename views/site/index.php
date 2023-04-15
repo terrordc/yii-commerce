6609a13ee634123e027ef7625d47555c6a8509f4
terrordc <nsctfkr@gmail.com>
2023-04-15 12:10:26 +0600

    изменил

--- views/site/index.php
+++ views/site/index.php
@@ -12,7 +12,7 @@ $this->title = 'My Yii Application';
 
        
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">ПРИВЕт</a></p>
     </div>
 
     <div class="body-content">
